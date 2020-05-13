c7f293b9a0a998cb113a43db56245f3d0db30024
Benoît Frisque <benoitfrisque@gmail.com>
2020-05-14 00:33:44 +0200

    Preparation of pypi package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages
 from distutils.core import setup
 
-version = "0.0.1"
+version = "0.1.1"
 
 with open('README.rst') as f:
     long_description = f.read()
@@ -12,21 +12,21 @@ with open('README.rst') as f:
 setup(name='ofxstatement-be-bnp',
       version=version,
       author="Benoît Frisque",
-      author_email="benoit.frisque@gmail.com",
-      url="",
-      description=("OFXStatement plugin for BNP Paribas Fortis (Belgium)"),
+      author_email="benoitfrisque@gmail.com",
+      url="https://github.com/bfrisque/ofxstatement-be-bnp",
+      description=("ofxstatement plugin for parsing Belgian BNP Paribas Fortis bank's CSV statements to OFX"),
       long_description=long_description,
       license="GPLv3",
-      keywords=["ofx", "banking", "statement"],
+      keywords=["ofx", "banking", "statement", "bnp", "csv"],
       classifiers=[
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 5 - Production/Stable',
           'Programming Language :: Python :: 3',
           'Natural Language :: English',
           'Topic :: Office/Business :: Financial :: Accounting',
           'Topic :: Utilities',
           'Environment :: Console',
           'Operating System :: OS Independent',
-          'License :: OSI Approved :: GNU Affero General Public License v3'],
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=["ofxstatement", "ofxstatement.plugins"],
